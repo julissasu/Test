d6c32fdf69ebac198f42f5c14f15560b71ac84d6
Julissa Su <157324701+julissasu@users.noreply.github.com>
2026-04-03 15:57:56 -0400

    Refactor prime checking and result printing logic

--- prime.cpp
+++ prime.cpp
@@ -4,25 +4,38 @@ using namespace std;
 // Function to check if a number is prime
 bool isPrime(int n) {
     if (n <= 1) return false;
-    
-    for (int i = 2; i <= n / 2; i++) {
+
+    for (int i = 2; i * i <= n; i++) {  // more efficient
         if (n % i == 0)
             return false;
     }
-    
+
     return true;
+}
+
+// Function to print result
+void printResult(int n) {
+    if (isPrime(n))
+        cout << n << " is a prime number.\n";
+    else
+        cout << n << " is not a prime number.\n";
 }
 
 int main() {
     int num;
+    char choice;
 
-    cout << "Enter a number: ";
-    cin >> num;
+    do {
+        cout << "Enter a number: ";
+        cin >> num;
 
-    if (isPrime(num))
-        cout << num << " is a prime number." << endl;
-    else
-        cout << num << " is not a prime number." << endl;
+        printResult(num);
 
+        cout << "Check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Goodbye!\n";
     return 0;
 }
