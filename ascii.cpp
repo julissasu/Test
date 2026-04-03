ed1fbee7ff7b62b34c880571e6809f3efb103b38
Julissa Su <157324701+julissasu@users.noreply.github.com>
2026-04-03 15:07:03 -0400

    ascii value of a character

---
+++ ascii.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c = 'k';
+
+    // (int)c converts the character to its ASCII integer value
+    cout << "The ASCII value of " << c << " is " << (int)c;
+
+    return 0;
+}
