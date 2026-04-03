4e4e6cbe5772a7057d61a76be674052340a2c43f
Julissa Su <157324701+julissasu@users.noreply.github.com>
2026-04-03 15:38:49 -0400

    Add hello_world.cpp to print greeting and sum

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3;
+    int sum = a + b;
+
+    cout << "Hello, World!" << endl;
+    cout << "Sum of " << a << " and " << b << " is " << sum << endl;
+
+    return 0;
+}
