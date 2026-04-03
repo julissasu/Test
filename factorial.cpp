9f1ddce72c70b6c368ec21263c2611bd478e479c
Julissa Su <157324701+julissasu@users.noreply.github.com>
2026-04-03 15:48:26 -0400

    Add factorial calculation program in C++

---
+++ factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
+    return 0;
+}
