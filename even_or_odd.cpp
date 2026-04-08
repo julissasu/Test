b8d7d4825fb74b2f99546f203115f14158b78f40
Julissa Su <157324701+julissasu@users.noreply.github.com>
2026-04-08 13:00:57 -0400

    Refactor even/odd check into separate function

--- even_or_odd.cpp
+++ even_or_odd.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 using namespace std;
 
+// Function to check even or odd
+bool isEven(int n) {
+    return n % 2 == 0;
+}
+
 int main() {
     int num;
+    char choice;
+    int evenCount = 0, oddCount = 0;
 
-    cout << "Enter a number: ";
-    cin >> num;
+    do {
+        cout << "Enter a number: ";
+        cin >> num;
 
-    if (num % 2 == 0)
-        cout << num << " is even." << endl;
-    else
-        cout << num << " is odd." << endl;
+        if (isEven(num)) {
+            cout << num << " is even." << endl;
+            evenCount++;
+        } else {
+            cout << num << " is odd." << endl;
+            oddCount++;
+        }
+
+        cout << "Check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "\nSummary:\n";
+    cout << "Even numbers entered: " << evenCount << endl;
+    cout << "Odd numbers entered: " << oddCount << endl;
 
     return 0;
 }
